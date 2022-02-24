f64547041d7ca4d0ef67f284dc2e18eba75d6da1
Sandip Sadhukhan <sandippakhanna@gmail.com>
2022-02-24 16:38:21 +0530

    page fix

--- frontend/components/profile/sections/blog/PaginationButton.tsx
+++ frontend/components/profile/sections/blog/PaginationButton.tsx
@@ -1,33 +1,31 @@
-import { Flex, Text } from "@chakra-ui/react"
-import Link from "next/link"
+import { Flex } from "@chakra-ui/react"
 import React from "react"
 
 interface PaginationButtonProps {
   grayBackground: string
   secondaryColor: string
   active?: boolean
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  children: any
 }
 
 const PaginationButton: React.FC<PaginationButtonProps> = (
   props: PaginationButtonProps
 ) => {
-  const { grayBackground, secondaryColor, active = false } = props
+  const { children, grayBackground, secondaryColor, active = false } = props
 
   return (
-    <Link href="#">
-      <a>
-        <Flex
-          justifyContent="center"
-          alignItems="center"
-          w={[8, 8, 10, 12, 14]}
-          h={[8, 8, 10, 12, 14]}
-          bgColor={active ? secondaryColor : grayBackground}
-          _hover={{ color: secondaryColor }}
-        >
-          {props.children}
-        </Flex>
-      </a>
-    </Link>
+    <Flex
+      justifyContent="center"
+      alignItems="center"
+      cursor={"pointer"}
+      w={[8, 8, 10, 12, 14]}
+      h={[8, 8, 10, 12, 14]}
+      bgColor={active ? secondaryColor : grayBackground}
+      _hover={{ color: active ? "white" : secondaryColor }}
+    >
+      {children}
+    </Flex>
   )
 }
 
