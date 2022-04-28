4bd84558bee9c40557e15e4dcff471749020a3b1
Sandip Sadhukhan <sandippakhanna@gmail.com>
2022-04-28 16:54:03 +0530

    update static files

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -171,6 +171,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "static_files"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "upload"
 
