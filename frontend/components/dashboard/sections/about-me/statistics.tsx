d1f99ec123d6da0b4f541d915b8a2b1185b71c84
Sandip Sadhukhan <sandippakhanna@gmail.com>
2022-07-05 20:59:35 +0530

    statistics done

--- frontend/components/dashboard/sections/about-me/statistics.tsx
+++ frontend/components/dashboard/sections/about-me/statistics.tsx
@@ -1,9 +1,94 @@
-import { Button, Divider, Flex, HStack, Input, Text } from "@chakra-ui/react"
-import React from "react"
+import {
+  Button,
+  Divider,
+  Flex,
+  HStack,
+  Input,
+  Text,
+  useToast,
+  VStack,
+} from "@chakra-ui/react"
+import React, { useEffect } from "react"
+import { SubmitHandler, useForm } from "react-hook-form"
+import { withAuth } from "../../../../auth/context"
+import { IState } from "../../../../types/auth"
+import axiosInstance from "../../../../utils/axiosInstance"
 
-const StatisticsContent = () => {
+interface IFormData {
+  projects: number
+  meetings: number
+  happy_clients: number
+  awards_won: number
+  experience: number
+}
+
+interface StatisticsContentProps {
+  state: IState
+}
+
+const StatisticsContent: React.FC<StatisticsContentProps> = (
+  props: StatisticsContentProps
+) => {
+  const token = props.state.user?.access as string
+  const toast = useToast()
+
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { isSubmitting },
+  } = useForm<IFormData>()
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axiosInstance.get(
+        "/api/dashboard/statistics-settings/",
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      )
+      const data: IFormData = response.data
+
+      setValue("projects", data.projects)
+      setValue("meetings", data.meetings)
+      setValue("happy_clients", data.happy_clients)
+      setValue("awards_won", data.awards_won)
+      setValue("experience", data.experience)
+    }
+
+    fetchData()
+  }, [token, setValue])
+
+  const onSubmit: SubmitHandler<IFormData> = async (data: IFormData) => {
+    const response = await axiosInstance.post(
+      "/api/dashboard/statistics-settings/",
+      data,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    )
+    const responseData: { message: string } = response.data
+
+    toast({
+      title: responseData.message,
+      status: "success",
+      duration: 3000,
+      isClosable: true,
+    })
+  }
+
   return (
-    <>
+    <VStack
+      as="form"
+      onSubmit={handleSubmit(onSubmit)}
+      w="full"
+      spacing={4}
+      align="start"
+    >
       <HStack
         align="start"
         w="full"
@@ -22,7 +107,14 @@ const StatisticsContent = () => {
           </Text>
         </Flex>
         <Flex flex={[1, 1, 8, 8, 10]} w="full">
-          <Input w="full" size="sm" placeholder="Ex - 300" />
+          <Input
+            type="number"
+            min={0}
+            w="full"
+            size="sm"
+            placeholder="Ex - 300"
+            {...register("projects")}
+          />
         </Flex>
       </HStack>
       <Divider />
@@ -45,7 +137,14 @@ const StatisticsContent = () => {
           </Text>
         </Flex>
         <Flex flex={[1, 1, 8, 8, 10]} w="full">
-          <Input w="full" size="sm" placeholder="Ex - 120" />
+          <Input
+            type="number"
+            min={0}
+            w="full"
+            size="sm"
+            placeholder="Ex - 120"
+            {...register("meetings")}
+          />
         </Flex>
       </HStack>
       <Divider />
@@ -68,7 +167,14 @@ const StatisticsContent = () => {
           </Text>
         </Flex>
         <Flex flex={[1, 1, 8, 8, 10]} w="full">
-          <Input w="full" size="sm" placeholder="Ex - 1200" />
+          <Input
+            type="number"
+            min={0}
+            w="full"
+            size="sm"
+            placeholder="Ex - 1200"
+            {...register("happy_clients")}
+          />
         </Flex>
       </HStack>
       <Divider />
@@ -91,7 +197,14 @@ const StatisticsContent = () => {
           </Text>
         </Flex>
         <Flex flex={[1, 1, 8, 8, 10]} w="full">
-          <Input w="full" size="sm" placeholder="Ex - 22" />
+          <Input
+            type="number"
+            min={0}
+            w="full"
+            size="sm"
+            placeholder="Ex - 22"
+            {...register("awards_won")}
+          />
         </Flex>
       </HStack>
       <Divider />
@@ -114,7 +227,14 @@ const StatisticsContent = () => {
           </Text>
         </Flex>
         <Flex flex={[1, 1, 8, 8, 10]} w="full">
-          <Input w="full" size="sm" placeholder="Ex - 5" />
+          <Input
+            type="number"
+            min={0}
+            w="full"
+            size="sm"
+            placeholder="Ex - 5"
+            {...register("experience")}
+          />
         </Flex>
       </HStack>
       <Divider />
@@ -123,15 +243,22 @@ const StatisticsContent = () => {
         ps={["full", "full", 110, 150, 175]}
         justifyContent={["start", "start", "end", "end", "end"]}
       >
-        <Button size="sm" rounded={0} colorScheme="green">
+        <Button
+          type="submit"
+          isLoading={isSubmitting}
+          loadingText="Saving"
+          size="sm"
+          rounded={0}
+          colorScheme="green"
+        >
           Save
         </Button>
         <Button size="sm" rounded={0} colorScheme="red">
           Cancel
         </Button>
       </HStack>
-    </>
+    </VStack>
   )
 }
 
-export default StatisticsContent
+export default withAuth(StatisticsContent)
