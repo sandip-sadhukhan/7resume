54db7923cde187191b1517e7df1da01dfab508c1
Sandip Sadhukhan <sandippakhanna@gmail.com>
2022-10-06 09:02:40 +0530

    blog category done

---
+++ frontend/components/dashboard/sections/blog/category/blog-category-skeleton.tsx
@@ -0,0 +1,35 @@
+import { HStack, Skeleton, Td, Tr } from "@chakra-ui/react"
+import React from "react"
+
+const BlogCategorySkeleton: React.FC = () => {
+  return (
+    <Tr w="full">
+      <Td w="28%">
+        <HStack
+          w="full"
+          align="start"
+          alignItems="center"
+          flexDir={["column", "row", "row", "row", "row"]}
+          gap={2}
+          spacing={[0, 2, 2, 2, 2]}
+        >
+          <Skeleton width="30%" height={3} />
+        </HStack>
+      </Td>
+      <Td>
+        <HStack
+          w="full"
+          align="start"
+          flexDir={["column", "row", "row", "row", "row"]}
+          gap={2}
+          spacing={[0, 2, 2, 2, 2]}
+        >
+          <Skeleton height={8} width={20} />
+          <Skeleton height={8} width={20} />
+        </HStack>
+      </Td>
+    </Tr>
+  )
+}
+
+export default BlogCategorySkeleton
--- frontend/components/dashboard/sections/blog/category/index.tsx
+++ frontend/components/dashboard/sections/blog/category/index.tsx
@@ -11,15 +11,78 @@ import {
   Thead,
   Tr,
   useColorModeValue,
+  useToast,
   VStack,
 } from "@chakra-ui/react"
 import Head from "next/head"
 import Link from "next/link"
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { FaPencilAlt, FaPlusSquare, FaTrash } from "react-icons/fa"
+import { withAuth } from "../../../../../auth/context"
+import { IState } from "../../../../../types/auth"
+import axiosInstance from "../../../../../utils/axiosInstance"
+import BlogCategorySkeleton from "./blog-category-skeleton"
 
-const BlogCategorySection = () => {
+interface BlogCategorySectionProps {
+  state: IState
+}
+const BlogCategorySection: React.FC<BlogCategorySectionProps> = (
+  props: BlogCategorySectionProps
+) => {
   const bgColor = useColorModeValue("white", "gray.700")
+  const token = props.state.user?.access as string
+  const toast = useToast()
+
+  interface BlogCategory {
+    id: number
+    title: string
+  }
+
+  const [data, setData] = useState<BlogCategory[] | null>(null)
+  const [isLoading, setLoading] = useState<boolean>(true)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axiosInstance.get(
+        "/api/dashboard/blog-categories/",
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      )
+      const data: BlogCategory[] = response.data
+      setData(data)
+      setLoading(false)
+    }
+
+    fetchData()
+  }, [token])
+
+  const deleteBlogCategory = async (id: number) => {
+    if (data === null) return
+
+    let newBlogCategory = [...data]
+    newBlogCategory = newBlogCategory.filter((blog) => blog.id !== id)
+    setData(newBlogCategory)
+
+    const response = await axiosInstance.delete(
+      `/api/dashboard/blog-category/${id}/`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    )
+    const resData: { message: string } = response.data
+
+    toast({
+      title: resData.message,
+      status: "success",
+      duration: 3000,
+      isClosable: true,
+    })
+  }
 
   return (
     <VStack
@@ -60,47 +123,59 @@ const BlogCategorySection = () => {
             </Tr>
           </Thead>
           <Tbody>
-            <Tr>
-              <Td>
-                <HStack
-                  w="full"
-                  align="start"
-                  alignItems="center"
-                  flexDir={["column", "row", "row", "row", "row"]}
-                  gap={2}
-                  spacing={[0, 2, 2, 2, 2]}
-                >
-                  <Text fontSize={13}>Busniess Logo</Text>
-                </HStack>
-              </Td>
+            {isLoading &&
+              [1, 2, 3].map((ele) => <BlogCategorySkeleton key={ele} />)}
+            {data &&
+              data.map((blogCategory) => (
+                <Tr key={blogCategory.id}>
+                  <Td>
+                    <HStack
+                      w="full"
+                      align="start"
+                      alignItems="center"
+                      flexDir={["column", "row", "row", "row", "row"]}
+                      gap={2}
+                      spacing={[0, 2, 2, 2, 2]}
+                    >
+                      <Text fontSize={13}>{blogCategory.title}</Text>
+                    </HStack>
+                  </Td>
 
-              <Td>
-                <HStack
-                  w="full"
-                  align="start"
-                  flexDir={["column", "row", "row", "row", "row"]}
-                  gap={2}
-                  spacing={[0, 2, 2, 2, 2]}
-                >
-                  <Link href="/dashboard/blog/categories/edit/1">
-                    <a>
-                      <Button size="sm" rounded={0} colorScheme="orange">
-                        <FaPencilAlt fontSize={13} />
+                  <Td>
+                    <HStack
+                      w="full"
+                      align="start"
+                      flexDir={["column", "row", "row", "row", "row"]}
+                      gap={2}
+                      spacing={[0, 2, 2, 2, 2]}
+                    >
+                      <Link
+                        href={`/dashboard/blog/categories/edit/${blogCategory.id}`}
+                      >
+                        <a>
+                          <Button size="sm" rounded={0} colorScheme="orange">
+                            <FaPencilAlt fontSize={13} />
+                            <Text fontSize={13} ps={2}>
+                              Edit
+                            </Text>
+                          </Button>
+                        </a>
+                      </Link>
+                      <Button
+                        size="sm"
+                        rounded={0}
+                        colorScheme="red"
+                        onClick={() => deleteBlogCategory(blogCategory.id)}
+                      >
+                        <FaTrash fontSize={13} />
                         <Text fontSize={13} ps={2}>
-                          Edit
+                          Delete
                         </Text>
                       </Button>
-                    </a>
-                  </Link>
-                  <Button size="sm" rounded={0} colorScheme="red">
-                    <FaTrash fontSize={13} />
-                    <Text fontSize={13} ps={2}>
-                      Delete
-                    </Text>
-                  </Button>
-                </HStack>
-              </Td>
-            </Tr>
+                    </HStack>
+                  </Td>
+                </Tr>
+              ))}
           </Tbody>
         </Table>
       </VStack>
@@ -108,4 +183,4 @@ const BlogCategorySection = () => {
   )
 }
 
-export default BlogCategorySection
+export default withAuth(BlogCategorySection)
