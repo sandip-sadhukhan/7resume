c01b4da4517f2336fd33df8bf656e5eb96e944cf
Sandip Sadhukhan <sandippakhanna@gmail.com>
2022-10-20 08:52:21 +0530

    update image location

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["localhost", "api.7resume.com"],
+    domains: ["localhost", "sandipsadhukhan.pythonanywhere.com"],
   },
 }
 
